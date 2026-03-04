6361b241380741ec8d51620ee1ce71d80a3b7d3d
med363 <blibechmohamedamine97@gmail.com>
2026-03-04 11:59:20 +0100

    init prject

---
+++ src/main/java/com/example/bookMgment/BookMgmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookMgment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMgmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMgmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookMgment/BookMgmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookMgment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMgmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
